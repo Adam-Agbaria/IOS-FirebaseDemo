9e2cad06ae0561a5024a7823469dca5fabb505c8
flash <dkflash@flashs-Mac.local>
2025-07-18 14:54:02 +0300

    Initial Commit

---
+++ AfekaFinal/AfekaFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfekaFinalApp.swift
+//  AfekaFinal
+//
+//  Created by flash on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AfekaFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfekaFinalTests/AfekaFinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfekaFinalTests.swift
+//  AfekaFinalTests
+//
+//  Created by flash on 18/07/2025.
+//
+
+import Testing
+@testable import AfekaFinal
+
+struct AfekaFinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
